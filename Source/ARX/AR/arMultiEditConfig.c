7f79a2750acfec83d7bc73b10336cd72257c41ca
Philip Lamb <phil.git@eden.net.nz>
2018-07-10 12:16:19 +1200

    Multimarker config editing.

---
+++ Source/ARX/AR/arMultiEditConfig.c
@@ -0,0 +1,178 @@
+/*
+ *  arMultiEditConfig.c
+ *  artoolkitX
+ *
+ *  This file is part of artoolkitX.
+ *
+ *  artoolkitX is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  artoolkitX is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with artoolkitX.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *  As a special exception, the copyright holders of this library give you
+ *  permission to link this library with independent modules to produce an
+ *  executable, regardless of the license terms of these independent modules, and to
+ *  copy and distribute the resulting executable under terms of your choice,
+ *  provided that you also meet, for each linked independent module, the terms and
+ *  conditions of the license of that module. An independent module is a module
+ *  which is neither derived from nor based on this library. If you modify this
+ *  library, you may extend this exception to your version of the library, but you
+ *  are not obligated to do so. If you do not wish to do so, delete this exception
+ *  statement from your version.
+ *
+ *  Copyright 2018 Philip Lamb
+ *  Copyright 2015 Daqri, LLC.
+ *  Copyright 2002-2015 ARToolworks, Inc.
+ *
+ *  Author(s): Hirokazu Kato, Philip Lamb
+ *
+ */
+
+#include <ARX/AR/ar.h>
+#include <ARX/AR/arMulti.h>
+#include <string.h> // memset()
+
+ARMultiMarkerInfoT *arMultiAllocConfig(void)
+{
+    ARMultiMarkerInfoT *marker_info = (ARMultiMarkerInfoT *)malloc(sizeof(ARMultiMarkerInfoT));
+    if (!marker_info) {
+        ARLOGe("arMultiAllocConfig out of memory!!\n");
+        return NULL;
+    }
+    
+    marker_info->marker = NULL;
+    marker_info->marker_num = 0;
+    marker_info->prevF = 0;
+    marker_info->patt_type = AR_MULTI_PATTERN_DETECTION_MODE_NONE;
+    marker_info->cfPattCutoff = AR_MULTI_CONFIDENCE_PATTERN_CUTOFF_DEFAULT;
+    marker_info->cfMatrixCutoff = AR_MULTI_CONFIDENCE_MATRIX_CUTOFF_DEFAULT;
+    marker_info->min_submarker = 0;
+    
+    return (marker_info);
+}
+
+// patt_type: Either AR_MULTI_PATTERN_TYPE_TEMPLATE or AR_MULTI_PATTERN_TYPE_MATRIX.
+int arMultiAddOrUpdateSubmarker(ARMultiMarkerInfoT *marker_info, int patt_id, int patt_type, ARdouble width, const ARdouble trans[3][4], uint64_t globalID)
+{
+    int i;
+    
+    // Look for matching marker already in set.
+    for (i = 0; i < marker_info->marker_num; i++) {
+        if (marker_info->marker[i].patt_type == patt_type && marker_info->marker[i].patt_id == patt_id) {
+            if (patt_type == AR_MULTI_PATTERN_TYPE_TEMPLATE || (patt_type == AR_MULTI_PATTERN_TYPE_MATRIX && marker_info->marker[i].globalID == globalID)) break;
+        }
+    }
+    
+    if (i == marker_info->marker_num) { // Not found, need to add to it.
+        
+        // Increase the array size.
+        ARMultiEachMarkerInfoT *emi;
+        if (!marker_info->marker) emi = (ARMultiEachMarkerInfoT *)malloc(sizeof(ARMultiEachMarkerInfoT));
+        else emi = (ARMultiEachMarkerInfoT *)realloc(marker_info->marker, sizeof(ARMultiEachMarkerInfoT) * (marker_info->marker_num + 1));
+        if (!emi) {
+            ARLOGe("arMultiAddOrUpdateSubmarker out of memory!!\n");
+            return (-1);
+        }
+        marker_info->marker = emi;
+        marker_info->marker_num++;
+
+        // As we've enlarged the array, i is now a valid index.
+        memset(&marker_info->marker[i], 0, sizeof(ARMultiEachMarkerInfoT));
+        marker_info->marker[i].patt_id = patt_id;
+        marker_info->marker[i].patt_type = patt_type;
+        marker_info->marker[i].width = width;
+        if (patt_type == AR_MULTI_PATTERN_TYPE_MATRIX) {
+            marker_info->marker[i].globalID = globalID;
+        }
+    }
+    
+    arMultiUpdateSubmarkerPose(&marker_info->marker[i], trans);
+    
+    if (patt_type == AR_MULTI_PATTERN_TYPE_MATRIX) {
+        if (marker_info->patt_type == AR_MULTI_PATTERN_DETECTION_MODE_TEMPLATE) {
+            marker_info->patt_type = AR_MULTI_PATTERN_DETECTION_MODE_TEMPLATE_AND_MATRIX;
+        } else {
+            marker_info->patt_type = AR_MULTI_PATTERN_DETECTION_MODE_MATRIX;
+        }
+    } else { // patt_type == AR_MULTI_PATTERN_TYPE_TEMPLATE
+        if (marker_info->patt_type == AR_MULTI_PATTERN_DETECTION_MODE_MATRIX) {
+            marker_info->patt_type = AR_MULTI_PATTERN_DETECTION_MODE_TEMPLATE_AND_MATRIX;
+        } else {
+            marker_info->patt_type = AR_MULTI_PATTERN_DETECTION_MODE_TEMPLATE;
+        }
+    }
+    
+    return 0;
+}
+
+void arMultiUpdateSubmarkerPose(ARMultiEachMarkerInfoT *submarker, const ARdouble trans[3][4])
+{
+    int i, j;
+    ARdouble wpos3d[4][2];
+    
+    for (j = 0; j < 3; j++) {
+        for (i = 0; i < 4; i++) {
+            submarker->trans[j][i] = trans[j][i];
+        }
+    }
+    
+    arUtilMatInv((const ARdouble (*)[4])submarker->trans, submarker->itrans);
+    
+    wpos3d[0][0] =  -submarker->width/2.0;
+    wpos3d[0][1] =   submarker->width/2.0;
+    wpos3d[1][0] =   submarker->width/2.0;
+    wpos3d[1][1] =   submarker->width/2.0;
+    wpos3d[2][0] =   submarker->width/2.0;
+    wpos3d[2][1] =  -submarker->width/2.0;
+    wpos3d[3][0] =  -submarker->width/2.0;
+    wpos3d[3][1] =  -submarker->width/2.0;
+    for (j = 0; j < 4; j++) {
+        submarker->pos3d[j][0] = submarker->trans[0][0] * wpos3d[j][0]
+                               + submarker->trans[0][1] * wpos3d[j][1]
+                               + submarker->trans[0][3];
+        submarker->pos3d[j][1] = submarker->trans[1][0] * wpos3d[j][0]
+                               + submarker->trans[1][1] * wpos3d[j][1]
+                               + submarker->trans[1][3];
+        submarker->pos3d[j][2] = submarker->trans[2][0] * wpos3d[j][0]
+                               + submarker->trans[2][1] * wpos3d[j][1]
+                               + submarker->trans[2][3];
+    }
+}
+
+int arMultiRemoveSubmarker(ARMultiMarkerInfoT *marker_info, int patt_id, int patt_type, uint64_t globalID)
+{
+    int i;
+    
+    // Look for matching marker already in set.
+    for (i = 0; i < marker_info->marker_num; i++) {
+        if (marker_info->marker[i].patt_type == patt_type && marker_info->marker[i].patt_id == patt_id) {
+            if (patt_type == AR_MULTI_PATTERN_TYPE_TEMPLATE || (patt_type == AR_MULTI_PATTERN_TYPE_MATRIX && marker_info->marker[i].globalID == globalID)) break;
+        }
+    }
+
+    if (i == marker_info->marker_num) return -1; // Not found.
+    
+    // Shuffle the rest down.
+    for (i++; i < marker_info->marker_num; i++) {
+        marker_info->marker[i - 1] = marker_info->marker[i];
+    }
+    
+    // Reduce the array size. Because we're shrinking, realloc failure isn't fatal.
+    ARMultiEachMarkerInfoT *emi = (ARMultiEachMarkerInfoT *)realloc(marker_info->marker, sizeof(ARMultiEachMarkerInfoT) * (marker_info->marker_num - 1));
+    if (!emi) {
+        ARLOGw("arMultiAddOrUpdateEachMarker out of memory!!\n.");
+    } else {
+        marker_info->marker = emi;
+    }
+    marker_info->marker_num--;
+
+    return 0;
+}
--- Source/ARX/AR/include/ARX/AR/arMulti.h
+++ Source/ARX/AR/include/ARX/AR/arMulti.h
@@ -59,6 +59,7 @@ extern "C" {
 #define    AR_MULTI_PATTERN_TYPE_TEMPLATE    0
 #define    AR_MULTI_PATTERN_TYPE_MATRIX      1
 
+#define    AR_MULTI_PATTERN_DETECTION_MODE_NONE -1
 #define    AR_MULTI_PATTERN_DETECTION_MODE_TEMPLATE 0
 #define    AR_MULTI_PATTERN_DETECTION_MODE_MATRIX 1
 #define    AR_MULTI_PATTERN_DETECTION_MODE_TEMPLATE_AND_MATRIX 2
@@ -70,29 +71,37 @@ extern "C" {
 
 
 typedef struct {
-    int     patt_id;
-    int     patt_type; // Either AR_MULTI_PATTERN_TYPE_TEMPLATE or AR_MULTI_PATTERN_TYPE_MATRIX.
-    ARdouble  width;
-    ARdouble  trans[3][4];
-    ARdouble  itrans[3][4];
-    ARdouble  pos3d[4][3];
-    int     visible;
-    int     visibleR;
-    uint64_t globalID;
+    int      patt_id;      // ID of the template pattern or matrix barcode.
+    int      patt_type;    // Type of this pattern. Either AR_MULTI_PATTERN_TYPE_TEMPLATE or AR_MULTI_PATTERN_TYPE_MATRIX.
+    ARdouble width;
+    ARdouble trans[3][4];  // Pose of this marker, expressed in multimarker coordinate system.
+    ARdouble itrans[3][4]; // Inverse of trans, i.e. pose of the multimarker, expressed in this marker's coordinate system.
+    ARdouble pos3d[4][3];  // Position of each corner (in order: upper-left, upper-right, lower-right, lower right), expressed in multimarker coordinate system.
+    int      visible;      // Used internally in arGetTransMatMultiSquare2/arGetTransMatMultiSquareStereo2. Set to index into ARMarkerInfo array of the matched marker, or -1 if no match.
+    int      visibleR;     // Used internally in arGetTransMatMultiSquareStereo2. Set to index into ARMarkerInfo array for the right camera of the matched marker, or -1 if no match.
+    uint64_t globalID;     // If patt_type == AR_MULTI_PATTERN_TYPE_MATRIX, the globalID of the matrix or 0 if not a global ID.
 } ARMultiEachMarkerInfoT;
 
 typedef struct {
-    ARMultiEachMarkerInfoT *marker;
-    int                     marker_num;
-    ARdouble                trans[3][4];
-    int                     prevF;
-    int                     patt_type;
-    ARdouble                cfPattCutoff;
-    ARdouble                cfMatrixCutoff;
-    int                     min_submarker;
+    ARMultiEachMarkerInfoT *marker;         // Array of markers in this set.
+    int                     marker_num;     // Number of markers present in this set (i.e. length of marker array).
+    ARdouble                trans[3][4];    // Transform (i.e. pose) of this multimarker set, expressed in camera coordinate system.
+    int                     prevF;          // 1 if the most recent call to arGetTransMatMulti estimated a valid pose. 0 otherwise.
+    int                     patt_type;      // Aggregate type of patterns present in this set. Either AR_MULTI_PATTERN_DETECTION_MODE_TEMPLATE or AR_MULTI_PATTERN_DETECTION_MODE_MATRIX or AR_MULTI_PATTERN_DETECTION_MODE_TEMPLATE_AND_MATRIX.
+    ARdouble                cfPattCutoff;   // Minimum matching confidence required for any pattern markers in order to consider them when calculating the multimarker pose. Default value is AR_MULTI_CONFIDENCE_PATTERN_CUTOFF_DEFAULT.
+    ARdouble                cfMatrixCutoff; // Minimum matching confidence required for any matrix markers in order to consider them when calculating the multimarker pose. Default value is AR_MULTI_CONFIDENCE_MATRIX_CUTOFF_DEFAULT.
+    int                     min_submarker;  // Minimum number of markers in this set that must be detected in order to consider it a valid multimarker detection.
 } ARMultiMarkerInfoT;
 
+AR_EXTERN ARMultiMarkerInfoT *arMultiAllocConfig(void);
+
 AR_EXTERN ARMultiMarkerInfoT *arMultiReadConfigFile( const char *filename, ARPattHandle *pattHandle );
+    
+AR_EXTERN int arMultiAddOrUpdateSubmarker(ARMultiMarkerInfoT *marker_info, int patt_id, int patt_type, ARdouble width, const ARdouble trans[3][4], uint64_t globalID);
+    
+AR_EXTERN void arMultiUpdateSubmarkerPose(ARMultiEachMarkerInfoT *submarker, const ARdouble trans[3][4]);
+    
+AR_EXTERN int arMultiRemoveSubmarker(ARMultiMarkerInfoT *marker_info, int patt_id, int patt_type, uint64_t globalID);
 
 AR_EXTERN int arMultiFreeConfig( ARMultiMarkerInfoT *config );
 
